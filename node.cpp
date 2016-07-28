b7b2ebf0d99a3be32c5196763c16df88b2475fc6
Joe Shutts <joe.shutts@gmx.co.uk>
2016-07-28 17:17:07 +0100

    Added jump instructions to runline

--- node.cpp
+++ node.cpp
@@ -18,7 +18,7 @@ public:
   void nodeSet(int8_t nodeId, int16_t number);
 };
 
-void arrow::arrow(int8_t nodeId1, int8_t nodeId2) {
+arrow::arrow(int8_t nodeId1, int8_t nodeId2) {
   nodeId[0] = nodeId1;
   nodeId[1] = nodeId2;
   
@@ -73,10 +73,11 @@ class node{
   uint8_t nodeId;
   arrow arrows[];
   
-  pair<bool, int16_t> getFromSrc(string src);
+  pair<int16_t, int16_t> getFromSrc(string src);
   
 public:
   node(uint8_t nodeId, arrow arrowArray[]);
+  map<string, uint8_t> labels;
   int16_t acc;
   int16_t bak;
   int8_t  pc;
@@ -85,13 +86,11 @@ public:
   bool runline();  
 };
 
-void node::node(uint8_t nId, arrow arrowArray[]) {
+node::node(uint8_t nId, arrow arrowArray[]) {
   nodeId = nId;
   arrows = arrowArray[]
-  // TODO – Ask Joe how to refer to local variables
-  acc = 0;
-  bak = 0;
-  // TODO – Not sure how a vector really works :\
+  this.acc = 0;
+  this.bak = 0;
 }
 
 bool node::runline(){
@@ -104,7 +103,7 @@ bool node::runline(){
   } else if(strncmp("MOV", line.c_str(), 3)){
     int c;
     std::string src, dst;
-    pair<bool, uint16_t> p;
+    pair<int8_t, uint16_t> p;
     int16_t input;
     
     operands = line.sub_str(4);
@@ -112,6 +111,7 @@ bool node::runline(){
     src = operands.substr(0,c);
     dst = operands.substr(c);
     p = getFromSrc(src);
+
     if (!p.first) 
       return true;
     
@@ -120,10 +120,11 @@ bool node::runline(){
     if(dst == "ACC") {
       acc = input;
     } else if(dst== "NIL") {
-      // I guess do nothing???
+      pc++;
+      return true;
+      // T:I guess do nothing??? J: nope
     } else if(dst== "LEFT":
       arrow a = arrows[3];
-      
       if (!a.nodeRequest(nodeId)) {
         a.nodeSet(nodeId, input);
       } else {
@@ -158,15 +159,17 @@ bool node::runline(){
     }
     pc++;
     return true;
-    
+
   } else if(strncmp("SWP", line.c_str(), 3)){
     int16_t tmpAcc = acc;
     acc = bak;
     bak = tmpAcc;
+    pc++;
     return true;
     
   } else if(strncmp("SAV", line.c_str(), 3)){
     bak = acc;
+    pc++;
     return true;
     
   } else if(strncmp("ADD", line.c_str(), 3)){
@@ -174,58 +177,80 @@ bool node::runline(){
     pair<bool, int16_t> p = getFromSrc(src);
     int16_t input;
     
-    if (!p.first) {
-      return false;
+    if (p.first==WAIT) {
+      return true;
     }
     
-    input = p.second;
-    acc += input;
-    return true;
-    
+    else if(p.first==INVALID){
+      //error
+      return false;
+    }
+    els{
+      input = p.second;
+      acc += input;
+      pc++;
+      return true;
+    }
   } else if(strncmp("SUB", line.c_str(), 3)){
     string src = line.sub_str(4);
-    pair<bool, int16_t> p = getFromSrc(src);
+    pair<int8_t, int16_t> p = getFromSrc(src);
     int16_t input;
-    
-    if (!p.first) {
-      return false;
+
+        
+    if (p.first==WAIT) {
+      return true;
     }
     
-    input = p.second;
-    
-    acc -= input;
-    return true;
+    else if(p.first==INVALID){
+      //error
+      return false;
+    }
+    els{
+      input = p.second;
+      acc -= input;
+      pc++;
+      return true;
+    }
     
   } else if(strncmp("NEG", line.c_str(), 3)){
     acc = -acc;
-    return true;
-    
-  } else if(strncmp("JRO", line.c_str(), 3)){
-    string src = line.sub_str(4);
-    pair<bool, int16_t> p = getFromSrc(src);
-    int16_t input;
-    
-    if (!p.first) {
+    pc++;
+    return true;   
+  }
+  else if(strncmp("JRO", line.c_str(), 3)){
+    pair<int8_t, int16_t> p = getFromSrc(src);
+      if(pair.first==WAIT)
+	return true;
+      else if(pair.first==SET)
+	pc=pc+pair.second;
+      return true;
+      pc=pc+p.second;
+  } 
+  else if(strncmp("J",line.c_str(),1)){
+    map<string, int8_t> ::iterator it = this.labels.find(line.sub_str(4));
+    if(it=this.labels.end())
       return false;
-    }
-    
-    input = p.second;
-    
-    pc += input;
+    if(strncmp("JGZ", line.c_str(), 3)&&this.acc>0)
+      pc=it->second;
+    else if(strncmp("JLZ", line.c_str(),3) &&acc<0)
+      pc=it->second;
+    else if(strncmp("JEZ", line.c_str(),3))
+      pc=it->second;
+    else if(strncmp("JNZ", line.c_str(),3)**acc!=0)
+      pc=it->second;
     return true;
-    
-  } else if(strncmp("J", line.c_str(), 1)){
-    // JMP, JEZ, JNZ, JGZ, JLZ
-  } else {
+  }
+  else {
     // TODO – Handle error
+    return false;
   }
 }
-
-pair<bool, int16_t> node::getFromSrc(string src) {
+  
+pair<int8_t, int16_t> node::getFromSrc(string src) {
   if(src.find_first_not_of("0123456789") == std::string::npos) {
     // Then src is just a number
     pair<bool, int16_t> p;
-    p.first = true;
+    p.first = SET;
     p.second = stoi(src);
     
     return p;
