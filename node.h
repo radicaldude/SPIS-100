da2d7b6f0b111b57124b2ceb91ccddce131d62a1
Joe Shutts <joe.shutts@gmx.co.uk>
2016-07-30 00:42:02 +0100

    Classes in their own files.

---
+++ node.h
@@ -0,0 +1,290 @@
+#include <cstdint>
+#include <utility>
+#include <string>
+#include <vector>
+
+#include "spis.h"
+
+using namespace std;
+
+const string SIM_OPS[4] = { "NOP", "SWP", "SAV", "NEG" };  // Operations with no parameters
+const string SRC_OPS[3] = { "ADD", "SUB", "JRO" };  // Operations with only SRC as a parameter
+const string SD_OPS[1] = { "MOV" };  // Operations with both SRC and DST as parameters
+const string LAB_OPS[3] = { "JMP", "JEZ", "JNZ", "JGZ", "JLZ"}; // Operations with only SRC as a parameter
+
+class io{
+ public:
+  arrow *arrow; 
+  std::vector<string> io;
+};
+
+class node{
+  uint8_t nodeId;
+  vector<arrow> arrows;
+  pair<bool, int16_t> getFromSrc(string src);
+ public:
+  WINDOW *w_main;
+  WINDOW *w_code;
+  WINDOW *w_reg;
+  node(uint8_t nId, vector<arrow> as);
+  int16_t acc;
+  int16_t bak;
+  int8_t  pc;
+  std::vector<string> code;
+  
+  bool runline();
+  bool runPrepare();
+};
+
+node::node(uint8_t nId, arrow arrowArray[]) {
+  this.nodeId = nId;
+  arrows = arrowArray[]
+  this.acc = 0;
+  this.bak = 0;
+}
+
+node::node(string *code){
+
+}
+
+bool node::runline(){
+  //string line = sanitize(code[this.pc]);
+  string line = code[pc];
+  // TODO – Remove labels and variations of commas, in sanitize function
+  
+  if(strncmp("NOP", line.c_str(), 3)){
+    pc++;
+    return true;
+  } else if(strncmp("MOV", line.c_str(), 3)){
+    int c;
+    std::string src, dst;
+    pair<int8_t, uint16_t> p;
+    int16_t input;
+
+    string operands = line.substr(4);
+    c = operands.find_first_of(' ');
+    src = operands.substr(0,c);
+    dst = operands.substr(c);
+    p = getFromSrc(src);
+    if(p.first==INVALID)
+      return false;
+    input = p.second;
+
+    if(dst == "ACC") {
+      acc = input;
+    } else if(dst== "NIL") {
+      pc++;
+      return true;
+      // T:I guess do nothing??? J: nope
+    } else if(dst== "LEFT":
+      arrow a = arrows[3];
+      if (!a.nodeRequest(nodeId)) {
+        a.nodeSet(nodeId, input);
+      } else {
+        pc--;
+      }
+    } else if(dst== "RIGHT") {
+      arrow a = arrows[1];
+
+      if (!a.nodeRequest(nodeId)) {
+        a.nodeSet(nodeId, input);
+      } else {
+        pc--;
+      }
+    } else if(dst== "UP") {
+      arrow a = arrows[0];
+
+      if (!a.nodeRequest(nodeId)) {
+        a.nodeSet(nodeId, input);
+      } else {
+        pc--;
+      }
+    } else if(dst== "DOWN") {
+      arrow a = arrows[2];
+
+      if (!a.nodeRequest(nodeId)) {
+        a.nodeSet(nodeId, input);
+      } else {
+        pc--;
+      }
+      // TODO – Handle error
+      return false;
+    }
+    pc++;
+    return true;
+
+  } else if(strncmp("SWP", line.c_str(), 3)){
+    int16_t tmpAcc = acc;
+    acc = bak;
+    bak = tmpAcc;
+    pc++;
+    return true;
+
+  } else if(strncmp("SAV", line.c_str(), 3)){
+    bak = acc;
+    pc++;
+    return true;
+
+  } else if(strncmp("ADD", line.c_str(), 3)){
+    string src = line.substr(4);
+    pair<bool, int16_t> p = getFromSrc(src);
+    int16_t input;
+    if (p.first==WAIT) {
+      return true;
+    }
+    
+    else if(p.first==INVALID){
+      //error
+      return false;
+    }
+    els{
+      input = p.second;
+      acc += input;
+      pc++;
+      return true;
+    }
+  } else if(strncmp("SUB", line.c_str(), 3)){
+    string src = line.sub_str(4);
+    pair<int8_t, int16_t> p = getFromSrc(src);
+    int16_t input;
+
+        
+    if (p.first==WAIT) {
+      return true;
+    }
+    
+    else if(p.first==INVALID){
+      //error
+      return false;
+    }
+    els{
+      input = p.second;
+      acc -= input;
+      pc++;
+      return true;
+    }
+    
+  } else if(strncmp("NEG", line.c_str(), 3)){
+    acc = -acc;
+    pc++;
+    return true;   
+  }
+  else if(strncmp("JRO", line.c_str(), 3)){
+    pair<int8_t, int16_t> p = getFromSrc(src);
+      if(pair.first==WAIT)
+	return true;
+      else if(pair.first==SET)
+	pc=pc+pair.second;
+      return true;
+      pc=pc+p.second;
+  } 
+  else if(strncmp("J",line.c_str(),1)){
+    map<string, int8_t> ::iterator it = this.labels.find(line.sub_str(4));
+    if(it=this.labels.end())
+      return false;
+    if(strncmp("JGZ", line.c_str(), 3)&&this.acc>0)
+      pc=it->second;
+    else if(strncmp("JLZ", line.c_str(),3) &&acc<0)
+      pc=it->second;
+    else if(strncmp("JEZ", line.c_str(),3))
+      pc=it->second;
+    else if(strncmp("JNZ", line.c_str(),3)**acc!=0)
+      pc=it->second;
+    else
+      pc++;
+    return true;
+  }
+  else
+    // TODO – Handle error
+    return false;
+}
+              
+bool node::runPrepare() {
+  // TODO – Sanetize
+  
+  // TODO – Debug
+  for (uint8_t i = 0; i < code.size(); a += 1) {
+    
+  }
+  
+  
+  // TODO – Collect labels
+  // TODO – Set properties for run line
+}
+
+
+pair<int8_t, int16_t> node::getFromSrc(string src) {
+  if(src.find_first_not_of("0123456789") == std::string::npos) {
+    // Then src is just a number
+    pair<bool, int16_t> p;
+    p.first = SET;
+    try{
+      p.second = stoi(src);
+    }
+    catch(...){
+      p.first = INVALID;
+      return p;
+    }
+    return p;
+  } else {
+    if (src == "ACC") {
+      pair<bool, int16_t> p;
+      p.first = SET;
+      p.second = this.acc;
+      return p;
+    } else if (src ==  "NIL") {
+      pair<bool, int16_t> p;
+      p.first = SET;
+      p.second = 0;
+      return p;
+    } else if (src ==  "LEFT") {
+      arrow a = arrows[3];
+      pair<bool, int16_t> p;
+
+      if (a.nodeRequest(nodeId)) {
+        p.first = SET;
+        p.second = a.nodeGet(nodeId);
+      } else {
+        p.first = WAIT;
+      }
+      return p;
+    } else if (src ==  "RIGHT") {
+      arrow a = arrows[1];
+      pair<bool, int16_t> p;
+
+      if (a.nodeRequest(nodeId)) {
+        p.first = SET;
+        p.second = a.nodeGet(nodeId);
+      } else {
+        p.first = WAIT;
+      }
+      return p;
+    } else if (src ==  "UP") {
+      arrow a = arrows[0];
+      pair<bool, int16_t> p;
+
+      if (a.nodeRequest(nodeId)){
+        p.first = SET;
+        p.second = a.nodeGet(nodeId);
+      } else {
+        p.first = WAIT;
+      }
+      return p;
+    } else if (src ==  "DOWN") {
+      arrow a = arrows[2];
+      pair<bool, int16_t> p;
+
+      if (a.nodeRequest(nodeId)) {
+        p.first = SET;
+        p.second = a.nodeGet(nodeId);
+      } else {
+        p.first = WAIT;
+      }
+      return p;
+    } else {
+      return p.first = INVALID;
+      // TODO – Handle error
+    }
+  }
+  // ANY and LAST are for the future (hopefully not)
+}
--- runtime.cpp
+++ runtime.cpp
@@ -1,26 +1,91 @@
 #include "spis.h"
-
-int compute_tick(std::vector<node> nodes, std::vector<arrow> inputs){
+#include "ui.h"
+#include <cncurses>
+  
+int compute_tick(std::vector<node> nodes, std::vector<io> inputs, std::vector<io> outputs){
   int i;
   
   for(i=0;i<inputs.size();i++){
-    if(inputs[0].set==0)
-      
-      }
+    if(inputs[0].arrow[0].status==WAIT){ //get next input
+
+    }
+  }
   for(i=0; i<nodes.size();i++){
-    std::vector<string> code=nodes[i].code;
-    for(int j=0;j<code.size();j++){
-      string line = sanitize(code[j]);
-      if(strncmp("MOV", line.c_str, 3)){
-	int c;
-	string src, dest;
-	
-	operands = line.sub_str(4);
-	c=operands.find_first_of(' ');
-	src = operands.substr(0,c);
-	dest = operands.substr(c);
-	if(
-	}
-      if(strncmp(
-    } 
+    node[i].runline();
+  }
+  for(i=0;i<outputs.size();i++){
+    //get output
+  }
+}
+
+int main(int argc, char **argv){
+  WINDOW *menu_win;
+  std::vector<WINDOW*> grid;
+  int max_y, max_x;
+
+  grid = malloc(sizeof(WINDOW)*grid_size[0]*grid_size[1]);
+  initscr();
+  curs_set(TRUE);
+  getmaxyx(stdscr, max_y, max_x);  
+  menu_win = new_win(max_y, menu_width, starty, startx);
+  init_level(&grid, grid_size, inputs, outputs);
+}
+
+void init_level(std::vector<node> grid, int *grid_size, std::vector<io> inputs, std::vector<io> outputs){
+  int c,x=MENU_WIDTH+BOX_WIDTH, y=0, id;
+  int max_x;
+  node tmp_node;
+  
+  getmaxyx(stdscr, y, max_x);
+  max_y=y;
+  for(int i=0, i<grid_size[0]; i++){
+    y=y+NODE_HEIGHT+2*GAP_WIDTH+ARROW_WIDTH;
+    for(int j=0, j<grid_size[1];j++){
+      grid.push_back(tmp_node());
+      tmp_node = grid[i*grid_size[0]+j];
+      tmp_node.w_main=new_win(NODE_HEIGHT, NODE_WIDTH, y, x);
+      tmp_node.w_code=new_win(NODE_HEIGHT, CODE_WIDTH-2, y, x+1);
+      tmp_node.w_reg=new_win(NODE_HEIGHT,NODE_WIDTH-CODE_WIDTH-2, y, x+CODE_WIDTH+1);
+      wprintw(tmp_node.w_reg, "ACC:%d\nBAK:%d", tmp_node.acc, tmp_node.bak);
+      wborder(tmp_node.w_reg, '|', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+      wborder(tmp_node.w_main, '|', '|', '-', '-', '+', '+', '+', '+');
+      x=x+NODE_WIDTH+ARROW_WIDTH+2*GAP_WIDTH;
+    }
+    y=y-NODE_HEIGHT-GAP_WIDTH;
+  }
+  c=0;
+  for(int i=0;i<grid_size[0];i++){
+    if(i=inputs[c].x){
+      inputs.arrow = grid[i].arrow[0];
+      c++;
+    }
+    if(c>inputs.size())
+      break;
+  }
+  for(int i=0;i<grid.size();i++){
+    y=max_y;
+    if(i%grid.size()==0){
+      x=MENU_WIDTH+GAP_WIDTH;
+      y=y-NODE_HEIGHT-GAP_WIDTH;
+    }
+    arrow[1]=new(arrow);
+    tmp_node.arrows[0].win=new_win(ARROW_HEIGHT, ARROW_WIDTH, y+BOX_HEIGHT/2+ARROW_HEIGHT/2, x+NODE_WIDTH+GAP);
+    arrow[2]=new(arrow);
+    tmp_node.arrow[2].win=new_win(ARROW_HEIGHT, ARROW_WIDTH, y-GAP, x+NODE_WIDTH/2);
+    if((int) i<grid_size[0]){
+      arrow[0]=new(arrow);
+      tmp_node.arrow[0]=new_win(ARROW_HEIGHT,ARROW_WIDTH,y+NODE_HEIGHT+GAP,x+NODE_WIDTH/2);
+    }
+    if(grid_size[0]%i==0){
+      arrow[3]=new(arrow);
+      tmp_node.arrow[3]=new_win(ARROW_HEIGHT,ARROW_WIDTH,y+NODE_HEIGHT/2,x-GAP-NODE_WIDTH);
+    }
+  }
+  return;
+}
+
+void update(){
+  
+  
+
 }
