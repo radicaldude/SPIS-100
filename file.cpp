72e4e6e2365578848ae8edc8eadcfb295d8e82e5
Joe Shutts <joe.shutts@gmx.co.uk>
2016-08-21 17:49:38 +0100

    x

--- file.cpp
+++ file.cpp
@@ -1,45 +1,51 @@
-#include "node.h"
-#include <iostream>
-#include <fstream>
-#define CODE_BUF
+#include "spis.h"
 
-int get_code(ifstream file, std::vector<node> grid){
+#define CODE_BUF 128
+
+int get_code(ifstream *file, std::vector<node> grid){
   char ch;
   char nID_buf[3];
   char code_buf[CODE_BUF];
-  int c=0,node;
+  int c=0,nID, j=0;
   
-  if(!file.open())
+  if(!file->is_open())
     return 1;
-  file.get(ch);
+  file->get(ch);
   while(ch!=EOF){
     for(int i=0;i<3;i++){
-      file.get(ch);
+      file->get(ch);
       if(ch>='0'&&ch<='9')
 	nID_buf[i]=ch;
       else
 	break;
     }
     nID=atoi(nID_buf);
-    if(!grid[nID])
+    if(nID>=grid.size())
       return 1;
     while(ch!='{'){
       if(ch==EOF)
 	return 1;
-      file.get(ch);
+      file->get(ch);
     }
-    code_buf[0]='/0';
-    c=0
-      while(ch!='}'){
-	if(ch==EOF)
-	  return 1;
-	if(c==CODE_BUF-1)
-	  return 1;
-	code_buf[c]=ch;
-	c++;
+    code_buf[0]='\0';
+    c=0;
+    while(ch!='}'){
+      if(ch==EOF)
+	return 1;
+      if(c==CODE_BUF-1)
+	return 1;
+      code_buf[c]=ch;
+      c++;
+    }
+    file->get(ch);
+    for(int i=0;i<c;i++){
+      char line_buf[MAX_LINE_LENGTH];
+      if(code_buf[i]=='\n'||code_buf[i]=='\r'){
+	j=i;
+	strncpy(line_buf,code_buf+j,i-j);
+	grid[nID].inputCode.push_back(std::string(line_buf));
       }
-    file.get(ch);
-    grid[nID].inputCode=string(code_buf);
+    }
   }
   return 0;
 }
